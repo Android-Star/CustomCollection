52141efdaeed0c964a66a33091d0e25c1eb07c09
hanwenxiao <hanwenxiao@unisound.com>
2019-01-15 17:10:59 +0800

    进度条绘制

---
+++ app/src/main/java/com/example/yzs/customcollection/showActivity/ScanActivity.java
@@ -0,0 +1,14 @@
+package com.example.yzs.customcollection.showActivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.example.yzs.customcollection.R;
+
+public class ScanActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.layout_scan);
+  }
+}
